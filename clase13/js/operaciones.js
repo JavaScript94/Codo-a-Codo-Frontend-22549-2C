6e3ae937f0f2130daec280f98a275e1950d84721
Javascript94 <elielambarri@gmail.com>
2022-10-10 11:17:30 -0300

    clase13:JavaScript Intro

---
+++ clase13/js/operaciones.js
@@ -0,0 +1,25 @@
+        /*comentario js =css*/
+        //comentario en js/
+        function saludar() {//funcion//
+            alert('hola mundo dentro de saludar()');
+        
+        }
+
+        /*OPERADORES*/
+        //CUENTAS MATEMATICAS
+        /*
+        +
+        -
+        /
+        *
+        */
+       //variable
+//sumar dos valores y mostrar su resultado
+let op1=prompt('ingrese el valor de op1'); //prompt me devuelve el valor entre comillas
+let op2=prompt('ingrese el valor de op2');
+let res; //declaro variable
+res = (+op1) + parseInt(op2)//; //asigno valor
+//muestro el resultado por consola
+console.log(res)
+//por ALERTA
+alert('resultado=' + res);//muestra cartel en patalla
